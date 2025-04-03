bef6456bea9cd0f3109f9b61b9ee9a5532e9858b
Lucas Lefèvre (lul) <lul@odoo.com>
2025-04-03 09:41:14 +0200

    adding panic hook to report errors

--- web_binding/src/wasm_tokenizer.rs
+++ web_binding/src/wasm_tokenizer.rs
@@ -1,9 +1,37 @@
-use tokenizer::tokenizer::parallel_tokenize as tok;
+use tokenizer::tokenizer::{parallel_tokenize, tokenize};
 use wasm_bindgen::{JsValue, prelude::wasm_bindgen};
+
+extern crate console_error_panic_hook;
 
 
 #[wasm_bindgen]
-pub fn parallel_tokenize(inputs: Vec<String>) -> Result<JsValue, serde_wasm_bindgen::Error> {
-    let tokens = tok(inputs);
+pub fn wasm_parallel_tokenize(inputs: Vec<String>) -> Result<JsValue, serde_wasm_bindgen::Error> {
+    set_panic_hook();
+    let tokens = parallel_tokenize(inputs);
     serde_wasm_bindgen::to_value(&tokens)
+}
+
+#[wasm_bindgen]
+pub fn wasm_batch_tokenize(inputs: Vec<String>) -> Result<JsValue, serde_wasm_bindgen::Error> {
+    set_panic_hook();
+    let tokens: Vec<_> = inputs.into_iter().map(|f| tokenize(&f)).collect();
+    serde_wasm_bindgen::to_value(&tokens)
+}
+
+#[wasm_bindgen]
+pub fn wasm_tokenize(input: String) -> Result<JsValue, serde_wasm_bindgen::Error> {
+    set_panic_hook();
+    let tokens = tokenize(&input);
+    serde_wasm_bindgen::to_value(&tokens)
+}
+
+pub fn set_panic_hook() {
+    // When the `console_error_panic_hook` feature is enabled, we can call the
+    // `set_panic_hook` function at least once during initialization, and then
+    // we will get better error messages if our code ever panics.
+    //
+    // For more details see
+    // https://github.com/rustwasm/console_error_panic_hook#readme
+    #[cfg(feature = "console_error_panic_hook")]
+    console_error_panic_hook::set_once();
 }
--- web_binding/web/src/index.ts
+++ web_binding/web/src/index.ts
@@ -1,7 +1,18 @@
-import init, { parallel_tokenize } from "wasm-hello-world/wasm_hello_world";
+import init, { wasm_batch_tokenize, wasm_parallel_tokenize, wasm_tokenize } from "wasm-hello-world/wasm_hello_world";
 
 await init();
 
-const tokens = parallel_tokenize(["=1+1", '="coucou" + 10']);
+wasm_parallel_tokenize
+wasm_batch_tokenize
+wasm_tokenize
+// console.log(wasm_parallel_tokenize(["=1+1", '="coucou" + 10']));
 
+let formulas: string[] = [];
+for (let i = 0; i < 100000; i++) {
+    formulas.push("=1+1");
+}
+console.time("wasm_tokenize")
+const tokens = wasm_batch_tokenize(formulas)
+console.timeEnd("wasm_tokenize")
 console.log(tokens)
+// console.log(tokens)
